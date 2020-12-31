090031905f7fdc6aef04815283aee4fc19a89d3f
unknown <maria.c.mury@gmail.com>
2020-12-31 15:11:41 -0500

    tests: add tests

---
+++ src/oata/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package oata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testWillAlwaysPass() {
+        assertEquals(true, true);
+    }
+
+}
